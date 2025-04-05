636bc5ca07ea56d0378d4b8df41591ae96dda92d
Dragoş <edragos33@gmail.com>
2025-04-05 16:44:03 +0200

    project started

---
+++ Fantasy Rpg game/Warrior.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warrior
+{
+    internal class Warrior
+    {
+        private string name;
+        private int health;
+        private int rage;
+        private int speed;
+        private string specialAttack;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set { health = 100; }
+        }
+
+        public int Rage
+        {
+            get { return rage; }
+            set { rage = value; }
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public string SpecialAttack
+        {
+            get { return specialAttack; }
+            set { specialAttack = value; }
+        }
+
+        public Warrior(string name, int health, int rage, int speed, string specialAttack)
+        {
+            this.name = name;
+            this.health = health;
+            this.rage = rage;
+            this.speed = speed;
+            this.specialAttack = specialAttack;
+        }
+    }
+}
---
+++ Fantasy Rpg game/Weapon.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_Rpg_game
+{
+    internal class Weapon
+    {
+        private string weaponName;
+        private string weaponType;
+        private int durability;
+        private int physicalDamage;
+        private int magicalDamage;
+        private string weaponRarity;
+
+
+        public string WeaponName
+        {
+            get { return weaponName; }
+            set { weaponName = value; }
+        }
+
+        public string Weapontype
+        {
+            get { return weaponType; }
+            set { weaponType = value; }
+        }
+
+        public int Durability
+        {
+            get { return durability; }
+            set { durability = 100; }
+        }
+
+        public int PhysicalDamage
+        {
+            get { return physicalDamage; }
+            set { physicalDamage = 0; }
+        }
+
+        public int MagicalDamage
+        {
+            get { return magicalDamage; }
+            set { magicalDamage = 0; }
+        }
+
+        public string WeaponRarity
+        {
+            get { return weaponRarity; }
+            set { weaponRarity = value; }
+        }
+
+
+
+        public Weapon(string weaponName, string weaponType, int durability, int physicalDamage, int magicalDamage, string weaponRarity)
+        {
+            this.weaponName = weaponName;
+            this.weaponType = weaponType;
+            this.durability = durability;
+            this.physicalDamage = physicalDamage;
+            this.magicalDamage = magicalDamage;
+            this.weaponRarity = weaponRarity;
+        }
+
+    }
+}
